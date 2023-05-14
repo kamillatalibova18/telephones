418ed7a48561cb53e33b57f1cf4a4d6d51d41c78
kamillatalibova18 <118733931+kamillatalibova18@users.noreply.github.com>
2023-05-14 23:54:34 +0300

    Add files via upload

---
+++ telephones/backup/edit.pas
@@ -0,0 +1,50 @@
+unit Edit;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons;
+
+type
+
+  { TfEdit }
+
+  TfEdit = class(TForm)
+    BitBtn1: TBitBtn;
+    bSave: TBitBtn;
+    CBNote: TComboBox;
+    eTelephone: TEdit;
+    eName: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure FormShow(Sender: TObject);
+    procedure Label1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  fEdit: TfEdit;
+
+implementation
+
+{$R *.lfm}
+
+{ TfEdit }
+
+procedure TfEdit.FormShow(Sender: TObject);
+begin
+  eName.SetFocus;
+end;
+
+
+
+
+
+end.
+
